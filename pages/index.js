58bfdbc980126d51edaefa99baab76c060cdd508
Dom-idk <fiscdo.df@gmail.com>
2021-04-27 21:05:12 +0000

    save 2

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
-function HomePage() {
-    return <div>Welcome to Next.js!</div>
+import Link from "next/link";
+
+
+  const HomePage = () => {
+    return <div>
+        Vitej na prvni strance
+        Spicy memiky zde:  {" "}
+        <Link href="/memes"><a>memes page</a></Link>
+    </div>
   }
   
   export default HomePage
-  
---
+++ pages/memes.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import Image from 'next/image'
+
+const MemesPage = () => {
+    return <div>
+      <a>Jsi na druhe strance</a>
+      Sem jsem chtěl vložit memísky 
+      </div>
+
+
+}
+  
+  export default MemesPage;
